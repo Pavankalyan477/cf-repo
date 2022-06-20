cedfce083cbc79c7df9806223f2efae7e7248922
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2022-06-20 17:29:37 +0530

    html,css assignment

---
+++ frontend/basic/Shopify_Project/JS/script.js
@@ -0,0 +1,33 @@
+
+function hide1(){
+    let data=document.querySelector(".h1");
+    if(data.style.display==="none"){
+        data.style.display="block";
+    }else{
+        data.style.display="none";
+    }
+}
+function hide2(){
+    let data=document.querySelector(".h2");
+    if(data.style.display==="none"){
+        data.style.display="block";
+    }else{
+        data.style.display="none";
+    }
+}
+function hide3(){
+    let data=document.querySelector(".h3");
+    if(data.style.display==="none"){
+        data.style.display="block";
+    }else{
+        data.style.display="none";
+    }
+}
+function hide4(){
+    let data=document.querySelector(".h4");
+    if(data.style.display==="none"){
+        data.style.display="block";
+    }else{
+        data.style.display="none";
+    }
+}
